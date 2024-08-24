66a003abd6ae83412c5aba19010dc80f23dea94e
KrishivDawra <krishivdawra@gmail.com>
2024-08-24 19:17:52 +0530

    header body footer created

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import Body from "./components/Body"
+import Footer from "./components/Footer"
+import Header from "./components/Header"
 
 function App() {
 
   return (
     <>
-      <h1>Hello ji</h1>
+      <Header/>
+      <Body/>
+      <Footer/>
     </>
   )
 }
